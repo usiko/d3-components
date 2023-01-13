082d57da0698227d4617270af0ea6d7f05a1693c
Quentin Ondet <61324827+usiko@users.noreply.github.com>
2023-01-13 16:05:36 +0100

    add img icon

--- src/app/components/graph-node/graph-node.model.ts
+++ src/app/components/graph-node/graph-node.model.ts
@@ -1,59 +1,75 @@
-import { ClusterNode, Edge, Node } from "@swimlane/ngx-graph";
+import { ClusterNode, Edge, Node } from '@swimlane/ngx-graph';
 
 export interface IGraphLink extends Edge {
-  meta?: {
-    color?: string,
-    width?: number,
-    markerEnd?: string,
-    cssClass?: string;
-  };
+	meta?: {
+		color?: string;
+		width?: number;
+		markerEnd?: string;
+		cssClass?: string;
+	};
 }
 
 export interface IGraphNode extends Node {
-  meta?: {
-    color: string,
+	meta?: {
+		color: string;
 
-    icon?: {
-      name?: string,
-      fontFamily?: string,
-      frontSize?: string,
-      color?: string,
-      cssClass?: string,
-    };
-    cssClass?: string,
-    label?: {
-      color?: string,
-      fontSize?: string,
-      fontFamily?: string,
-      cssClass?: string,
-    };
-  };
-  data: {
-    value: number;
-  };
+		icon?: {
+			font: {
+				fontFamily?: string;
+				frontSize?: string;
+				color?: string;
+				name: string;
+			};
+			
+			image: {
+				height?: number;
+				wdith?: number;
+				src: number;
+			};
 
+			cssClass?: string;
+		};
+		cssClass?: string;
+		label?: {
+			color?: string;
+			fontSize?: string;
+			fontFamily?: string;
+			cssClass?: string;
+		};
+	};
+	data: {
+		value: number;
+	};
 }
 export interface IGraphClusterNode extends ClusterNode {
-  meta?: {
-    color: string,
+	meta?: {
+		color: string;
 
-    icon?: {
-      name?: string,
-      fontFamily?: string,
-      frontSize?: string,
-      color?: string,
-      cssClass?: string,
-    };
-    cssClass?: string,
-    label?: {
-      color?: string,
-      fontSize?: string,
-      fontFamily?: string,
-      cssClass?: string,
-    };
-  };
-  data?: {
-    value: number;
-  };
+        icon?: {
+			font: {
+				fontFamily?: string;
+				frontSize?: string;
+				color?: string;
+				name: string;
+			};
+			
+			image: {
+				height?: number;
+				wdith?: number;
+				src: number;
+			};
 
+			cssClass?: string;
+		};
+		cssClass?: string;
+		label?: {
+			color?: string;
+			fontSize?: string;
+			fontFamily?: string;
+			cssClass?: string;
+		};
+	};
+	data?: {
+		value: number;
+	};
 }
