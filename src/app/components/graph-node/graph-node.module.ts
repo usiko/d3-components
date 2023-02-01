7136ca68a321fdf1f54c1ee745f0f2c8374ae2cf
Quentin Ondet <61324827+usiko@users.noreply.github.com>
2023-02-01 16:57:13 +0100

    blindcoding ngx popper dynamic template

--- src/app/components/graph-node/graph-node.module.ts
+++ src/app/components/graph-node/graph-node.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { NgxPopperModule } from 'ngx-popper';
 import { GraphNodeComponent } from './graph-node.component';
 import { NodeTemplateComponent } from './template/node-template/node-template.component';
 
@@ -16,6 +17,7 @@ import { NodeTemplateComponent } from './template/node-template/node-template.co
     imports: [
         BrowserModule,
         NgxGraphModule,
+        NgxPopperModule.forRoot({}),
         CommonModule
     ],
     exports: [
--- src/app/components/graph-node/template/node-template/node-template.component.ts
+++ src/app/components/graph-node/template/node-template/node-template.component.ts
@@ -2,24 +2,28 @@ import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@ang
 import { IGraphNode } from '../../graph-node.model';
 
 @Component({
-    selector: 'app-node-template',
-    templateUrl: './node-template.component.html',
-    styleUrls: ['./node-template.component.scss']
+	selector: 'app-node-template',
+	templateUrl: './node-template.component.html',
+	styleUrls: ['./node-template.component.scss'],
 })
 export class NodeTemplateComponent implements OnInit {
+	@Input() node!: IGraphNode;
+	@Input() popperContent?: PopperContent;
+	@Output() hover = new EventEmitter<never>();
+	@Output() dblClick = new EventEmitter<never>();
+	constructor(private element: ElementRef) {}
 
-    @Input() node!: IGraphNode;
-    @Output() dblClick = new EventEmitter<never>()
-    constructor(private element: ElementRef) { }
+	ngOnInit(): void {
+		if (this.node.meta?.icon?.svg?.color) {
+			this.element.nativeElement.querySelector('.icon.img');
+		}
+	}
+	nodeHover() {
+		console.log('mouseenter');
+		this.hover.emit();
+	}
 
-    ngOnInit(): void {
-        if (this.node.meta?.icon?.svg?.color) {
-            this.element.nativeElement.querySelector('.icon.img')
-        }
-    }
-
-    onDblclick() {
-        this.dblClick.emit();
-    }
-
+	onDblclick() {
+		this.dblClick.emit();
+	}
 }
